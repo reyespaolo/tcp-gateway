eb45287130f99bc8295fac5ad29f9b60bb76a371
Paolo Reyes <reyes.paolo@360logix.com>
2017-07-15 12:15:27 +0800

    Updates

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
 var net = require('net');
-
 server = net.createServer()
 
-server.once('error', (error) => {
-    console.error('TCP Gateway Error', error)
-    setTimeout(() => {
-      server.close(() => {
-        server.removeAllListeners()
-        process.exit()
-      })
-    }, 5000)
-  });
-
-  server.once('listening', () => {
+server.once('listening', () => {
     console.log("TCP Gateway Ok")
-  });
-
-  server.once('close', function () {
-    console.log("TCP Gateway Close")
- })
+});
 
  server.on('connection', (socket) => {
-     socket.setEncoding('utf8')
-     socket.setKeepAlive(true, 5000)
-     socket.setTimeout(3600000)
-     socket.on('error', (error) => {
-       console.error('Client Error.', error)
-       socket.destroy()
-     })
-
-     socket.on('timeout', () => {
-       plugin.log('TCP Gateway - Socket Timeout.')
-       socket.destroy()
-     })
-
-     socket.on('close', () => {
-       if (socket.device) {
-         delete clients[socket.device]
-       }
-       setTimeout(() => {
-         socket.removeAllListeners()
-       }, 5000)
-     })
-     socket.on('data', (data) => {
-       //Handle Data
-       console.log(data);
-
-     })
-
+   socket.setEncoding('utf8')
+   socket.setKeepAlive(true, 5000)
+   socket.setTimeout(3600000)
+   socket.on('error', (error) => {
+     console.error('Error.', error)
+     socket.destroy()
    })
 
-  server.listen(3111);
+   socket.on('timeout', () => {
+     plugin.log('TCP Gateway Timeout.')
+     socket.destroy()
+   })
+
+   socket.on('close', () => {
+     if (socket.device) {
+       delete clients[socket.device]
+     }
+     setTimeout(() => {
+       socket.removeAllListeners()
+     }, 5000)
+   })
+   socket.on('data', (data) => {
+     console.log(data);
+   })
+ })
+
+ server.once('close', function () {
+   console.log("TCP Gateway Close")
+ })
+
+ server.once('error', (error) => {
+  console.error('TCP Gateway Error', error)
+  setTimeout(() => {
+    server.close(() => {
+      server.removeAllListeners()
+      process.exit()
+    })
+  }, 5000)
+ });
+ server.listen(3111);
